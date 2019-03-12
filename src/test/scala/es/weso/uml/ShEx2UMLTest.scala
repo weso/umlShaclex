0bf4bb119c6be2d51db354e3ca22115156dfdef6
Jose Emilio Labra Gayo <jelabra@gmail.com>
2019-03-12 07:18:46 +0100

    First implementation of inlineOrs as separated UML entities

--- src/test/scala/es/weso/uml/ShEx2UMLTest.scala
+++ src/test/scala/es/weso/uml/ShEx2UMLTest.scala
@@ -1,31 +1,14 @@
 package es.weso.uml
-
 import es.weso.rdf.PREFIXES._
 import es.weso.rdf.nodes.IRI
 import es.weso.shex.{IRILabel, Schema}
-import es.weso.uml.UMLDiagram._
 import es.weso.utils.FileUtils
 import org.scalatest.{FunSpec, Matchers}
 
 class ShExUMLTest extends FunSpec with Matchers {
 
-  describe("Dot") {
-    it("Should generate plantuml diagram") {
-      val field1 = UMLField("xsd:name", Some("http://schema.org/name"), List(DatatypeConstraint("xsd:string","http;//xmlschema.org/string")), Range(1,Unbounded))
-      val field2 = UMLField("xsd:age", Some("http://schema.org/age"), List(DatatypeConstraint("xsd:int","http;//xmlschema.org/int")), Range(1,IntMax(1)))
-      val field3 = UMLField("xsd:homePage", Some("http://schema.org/homePage"), List(Constant("IRI")), Range(1,IntMax(1)))
-      val cls1 = UMLClass(1, ":User", Some("http://schema.org/User"), List(List(field1, field2)),List())
-      val cls2 = UMLClass(2, ":Company", Some("http://schema.org/User"), List(List(field3)), List())
-      val link1 = UMLLink(1,2,"schema:worksFor","http://schema.org", Star)
-      val uml = UML(Map(IRILabel(IRI("L1")) -> 1, IRILabel(IRI("L2")) -> 2), Map(1 -> cls1, 2 -> cls2), List(link1))
-      uml.classes.size should be(2)
-      uml.links.length should be(1)
-      // println(uml.toPlantUML)
-    }
-  }
-
   describe(s"ShEx2UML") {
-    it(s"Should convert simple Shape with IRI") {
+/*    it(s"Should convert simple Shape with IRI") {
       val shexStr =
         """|prefix : <http://example.org/>
            |
@@ -119,6 +102,31 @@ class ShExUMLTest extends FunSpec with Matchers {
         e => fail(s"Error converting to UML: $e"),
         uml => {
           info(s"FHIR schema converted to UML")
+        }
+      )
+    }
+*/
+    it(s"Should convert a Shape with OR and self-reference") {
+      val shexStr =
+        """|prefix : <http://example.org/>
+           |
+           |:User {
+           | :knows (@:User OR IRI)? ;
+           |}
+        """.stripMargin
+      val ex = IRI(s"http://example.org/")
+      val umlClass = UMLClass(0,":User", Some((ex + "User").str), List(), List())
+      val uml = UML(Map(IRILabel(ex + "User") -> 0), Map(0 -> umlClass), List())
+      val maybe = for {
+        shex <- Schema.fromString(shexStr,"ShExC")
+        uml <- ShEx2UML.schema2Uml(shex)
+      } yield uml
+      maybe.fold(
+        e => fail(s"Error converting to UML: $e"),
+        umlConverted => {
+          info(s"Expected: \n$uml\nObtained:\n$umlConverted")
+          uml should be(umlConverted)
+          uml.toSVG should include ("<svg")
         }
       )
     }
---
+++ src/test/scala/es/weso/uml/UMLTest.scala
@@ -0,0 +1,27 @@
+package es.weso.uml
+
+import es.weso.rdf.PREFIXES._
+import es.weso.rdf.nodes.IRI
+import es.weso.shex.{IRILabel, Schema}
+import es.weso.uml.UMLDiagram._
+import es.weso.utils.FileUtils
+import org.scalatest.{FunSpec, Matchers}
+
+class UMLTest extends FunSpec with Matchers {
+
+  describe("Serialize PlantUML") {
+    it("Should generate plantuml diagram") {
+      val field1 = UMLField("xsd:name", Some("http://schema.org/name"), List(DatatypeConstraint("xsd:string","http;//xmlschema.org/string")), Range(1,Unbounded))
+      val field2 = UMLField("xsd:age", Some("http://schema.org/age"), List(DatatypeConstraint("xsd:int","http;//xmlschema.org/int")), Range(1,IntMax(1)))
+      val field3 = UMLField("xsd:homePage", Some("http://schema.org/homePage"), List(Constant("IRI")), Range(1,IntMax(1)))
+      val cls1 = UMLClass(1, ":User", Some("http://schema.org/User"), List(List(field1, field2)),List())
+      val cls2 = UMLClass(2, ":Company", Some("http://schema.org/User"), List(List(field3)), List())
+      val link1 = UMLLink(1,2,"schema:worksFor","http://schema.org", Star)
+      val uml = UML(Map(IRILabel(IRI("L1")) -> 1, IRILabel(IRI("L2")) -> 2), Map(1 -> cls1, 2 -> cls2), List(link1))
+      uml.components.size should be(2)
+      uml.links.length should be(1)
+      println(uml.toPlantUML)
+    }
+  }
+
+}
